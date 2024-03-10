c19b5fc8eb4d66debb0083ebe55865722cacb32a
boomboompsh <boomboompsh@users.noreply.github.com>
2024-03-10 04:11:35 +0000

    t

--- esphome/components/ct_clamp/text_sensor.py
+++ esphome/components/ct_clamp/text_sensor.py
@@ -18,7 +18,7 @@ CONFIG_SCHEMA = (
     text_sensor.text_sensor_schema()
     .extend(
         {
-            cv.Required(CONF_SENSOR): cv.use_id(voltage_sampler.VoltageSampler),
+            #cv.Required(CONF_SENSOR): cv.use_id(voltage_sampler.VoltageSampler),
             cv.Optional(
                 CONF_SAMPLE_DURATION, default="200ms"
             ): cv.positive_time_period_milliseconds,
