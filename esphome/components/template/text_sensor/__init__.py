3b92252a8567bd94dc2d89b7f23a917602e4c486
boomboompsh <boomboompsh@users.noreply.github.com>
2024-03-10 01:01:14 +0000

    Update __init__.py
    add debug info

--- esphome/components/template/text_sensor/__init__.py
+++ esphome/components/template/text_sensor/__init__.py
@@ -46,6 +46,8 @@ async def to_code(config):
 async def text_sensor_template_publish_to_code(config, action_id, template_arg, args):
     paren = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, paren)
+    print(var)
+    print(config)
     template_ = await cg.templatable(config[CONF_STATE], args, cg.std_string)
     cg.add(var.set_state(template_))
     return var
